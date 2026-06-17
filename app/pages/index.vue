c3099342b3550a26237b8ac1ff19ad3f5139b981
yqc0911 <yanqic0911@gmail.com>
2026-06-17 21:46:15 +0800

    添加相关界面

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        我是首页
+    </div>
+</template>
+
+<script setup>
+
+</script>
+<style scoped lang="scss">
+
+</style>
---
+++ app/pages/me.vue
@@ -0,0 +1,12 @@
+<template>
+        <div>
+            我是个人主页
+        </div>
+</template>
+
+<script setup>
+
+</script>
+<style scoped lang="scss">
+
+</style>
